b8a4606284c752bb7e5c4bdcfc6ee28f78c82e6f
crazyreleaser <crazyreleaser@yandex.ru>
2021-09-02 12:36:42 +0300

    Initial Commit

---
+++ SimpleNavIOS/SimpleNavIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNavIOSApp.swift
+//  SimpleNavIOS
+//
+//  Created by admin on 02.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNavIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
